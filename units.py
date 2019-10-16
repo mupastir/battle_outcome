e1e98314ec7fc36854b1d518caf8f2ee46dc075f
oleg <mupastir@gmail.com>
2019-10-16 10:48:16 +0300

    units init pipfile

--- units.py
+++ units.py
@@ -1,17 +1,35 @@
 import random
+import numpy as np
+from abc import ABC, abstractmethod
+import typing
 
 
-class BaseUnit:
+class BaseUnit(ABC):
 
-    def __init__(self, recharge):
+    def __init__(self, recharge: int):
+        super(BaseUnit, self).__init__()
         self.health = 100
         self.recharge = recharge
 
+    @abstractmethod
+    def attack(self):
+        pass
 
-class Soldier(BaseUnit):
+    @abstractmethod
+    def damage(self):
+        pass
 
-    def __init__(self, recharge):
-        super(Soldier, self).__init__(recharge)
+    @property
+    def alive(self):
+        if self.health <= 0:
+            return False
+        return True
+
+
+class Soldier(BaseUnit, ABC):
+
+    def __init__(self, recharge: int):
+        super().__init__(recharge)
         self.experience = 0
 
     def up_experience(self, func):
@@ -22,16 +40,50 @@ class Soldier(BaseUnit):
 
         return decorated
 
-    def attack(self):
+    def attack_success(self):
         return 0.5 * (1 + self.health / 100) * random.randint(
             50 + self.experience, 100) / 100
 
     def damage(self):
         return 0.05 + self.experience / 100
 
+    def damaged(self, damage):
+        self.health -= damage
 
-class Vehicle(BaseUnit):
 
-    def __init__(self, recharge, *operators):
-        super(Vehicle, self).__init__(recharge)
-        self.operators = operators
+class Vehicle(BaseUnit, ABC):
+    OPERATORS_COUNT = 3
+
+    def __init__(self, recharge: int, *operators: typing.Iterable[Soldier]):
+        super().__init__(recharge)
+        self.operators = self._get_operators(operators)
+        self.total_health = self.health + (sum(self._operators_health)
+                                           / len(self._operators_health))
+
+    def _get_operators(self, operators):
+        if len(operators) != self.OPERATORS_COUNT:
+            raise ValueError
+
+    @property
+    def _operators_health(self):
+        return [x.health for x in self.operators]
+
+    @property
+    def _geometric_avg(self, *attack_success):
+        a = np.array(attack_success)
+        return a.prod() ** (1.0 / len(a))
+
+    def attack(self):
+        return (0.5 * (1 + self.health / 100)
+                * self._geometric_avg(
+                    [x.attack_success for x in self.operators]))
+
+    def damage(self):
+        return 0.1 + sum([x.experience / 100 for x in self.operators])
+
+    def damaged(self, damage):
+        self.health -= damage * 60 / 100
+        operators = self.operators
+        operators.pop([random.randint(0, 3)]).health -= damage * 20 / 100
+        for x in operators:
+            x.health -= damage * 10 / 100
