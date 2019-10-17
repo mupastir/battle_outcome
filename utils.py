b54452dabb100b29f024dc619faef6b563e301be
oleg <mupastir@gmail.com>
2019-10-17 16:59:37 +0300

    add exceptions

--- utils.py
+++ utils.py
@@ -3,6 +3,14 @@ from functools import reduce
 from _operator import mul
 
 
-def geometric_avg(*args):
-    prod = reduce(mul, *args, 0)
+class MaxExperienceException(Exception):
+    pass
+
+
+class UnitsNumber(Exception):
+    pass
+
+
+def geometric_avg(args):
+    prod = reduce(mul, args, 0)
     return prod ** (1.0 / len(args))
