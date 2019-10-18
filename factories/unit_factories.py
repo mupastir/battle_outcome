4720e7228a8263aa1fe8aab0112e5fbb1e51a13e
oleg <mupastir@gmail.com>
2019-10-18 18:32:43 +0300

    init unit_factories

---
+++ factories/unit_factories.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+from models.units import Soldier, Vehicle
+
+
+class UnitCreator(ABC):
+
+    @abstractmethod
+    def create(self):
+        pass
+
+
+class SoldierFactory(UnitCreator):
+
+    def __init__(self, recharge):
+        self.recharge = recharge
+
+    def create(self) -> Soldier:
+        return Soldier(self.recharge)
+
+
+class VehiclesFactory(UnitCreator):
+    OPERATORS_NUMBER = 3
+    OPERATORS_RECHARGE = 100
+
+    def __init__(self, recharge):
+        self.recharge = recharge
+
+    def create(self) -> Vehicle:
+        operators = [SoldierFactory(self.recharge).create()
+                     for i in range(self.OPERATORS_NUMBER)]
+        return Vehicle(self.recharge, operators)
