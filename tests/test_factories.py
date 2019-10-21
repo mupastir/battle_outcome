22795fc740d9377f15cb0fd202d64116d4e2f6c6
oleg <mupastir@gmail.com>
2019-10-21 09:02:42 +0300

    init factories tests

---
+++ tests/test_factories.py
@@ -0,0 +1,38 @@
+import pytest
+from factories.squad_factory import SquadFactory
+from factories.unit_factories import SoldierFactory, VehiclesFactory
+from models.squads import Squad
+from models.units import Soldier, Vehicle
+from utils import UnitsNumber
+
+RECHARGE_NORMAL = 100
+RECHARGE_VEHICLE = 1000
+UNITS_NUMBER = 7
+MIN_UNITS_NUMBER = 2
+
+
+class TestFactories:
+
+    def test_create_soldier(self):
+        soldier = SoldierFactory(RECHARGE_NORMAL).create()
+        assert isinstance(soldier, Soldier)
+
+    def test_create_vehicle(self):
+        vehicle = VehiclesFactory(RECHARGE_VEHICLE).create()
+        assert isinstance(vehicle, Vehicle)
+
+    def test_create_squad(self):
+        squad = SquadFactory(UNITS_NUMBER).create()
+        assert isinstance(squad, Squad)
+
+    def test_not_create_soldier(self):
+        with pytest.raises(TypeError):
+            SoldierFactory('').create()
+
+    def test_not_create_vehicle(self):
+        with pytest.raises(TypeError):
+            VehiclesFactory('').create()
+
+    def test_not_create_squad(self):
+        with pytest.raises(UnitsNumber):
+            SquadFactory(MIN_UNITS_NUMBER).create()
