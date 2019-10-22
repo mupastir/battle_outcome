8ed1c3a62106588a4143c9364a74be585999c821
oleg <mupastir@gmail.com>
2019-10-22 14:26:37 +0300

    initialize strategy classes

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 from simulation import RandomStrategy, StrongestStrategy, WeakestStrategy
 
-ATTACK_STRATEGIES = {1: RandomStrategy,
-                     2: WeakestStrategy,
-                     3: StrongestStrategy}
+ATTACK_STRATEGIES = {1: RandomStrategy(),
+                     2: WeakestStrategy(),
+                     3: StrongestStrategy()}
