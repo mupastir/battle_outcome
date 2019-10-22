8665ad0852c8482596c2a889ce1277a7e9b40685
oleg <mupastir@gmail.com>
2019-10-22 14:27:57 +0300

    change output logger to console update messages

--- console_user_interface.py
+++ console_user_interface.py
@@ -1,30 +1,33 @@
 import logging
+import sys
 from typing import Dict, List
 
-from messages import (armies_number_msg, damaged_not_success, damaged_success,
-                      name_army_entry_msg, number_of_squads_msg,
-                      number_of_units_msg, repeat_input, uncorrected_number)
+from messages import (armies_number_msg, attacking_msg, damaged_not_success,
+                      damaged_success, exception_msg, name_army_entry_msg,
+                      number_of_squads_msg, number_of_units_msg, repeat_input,
+                      strategy_choose_msg, uncorrected_number, winner_msg)
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class View:
+class ConsoleUserInterface:
 
     def _input_number(self, string_explanation: str) -> int:
         number = input(string_explanation)
         return self._validate_input(number)
 
     @staticmethod
-    def damaged_info(damage: int):
+    def damaged_info(attacking_army: str, damage: float):
         if damage > 0:
-            logging.info(damaged_success.format(damage))
+            logging.info(damaged_success.format(attacking_army, damage))
         else:
-            logging.info(damaged_not_success)
+            logging.info(damaged_not_success.format(attacking_army))
 
     def _validate_input(self, number: str) -> int:
         try:
             return int(number)
-        except TypeError:
+        except (ValueError, TypeError):
             logger.info(uncorrected_number)
             self._input_number(repeat_input)
 
@@ -38,9 +41,25 @@ class View:
         squads_list = self._input_squads(number_of_squads)
         return {name_army: squads_list}
 
-    def input_armies(self) -> Dict[str, List[int]]:
+    def get_armies(self) -> Dict[str, List[int]]:
         armies_dict = {}
         armies_number = self._input_number(armies_number_msg)
         for army in range(armies_number):
             armies_dict.update(self._input_army())
         return armies_dict
+
+    def get_strategy(self) -> int:
+        strategy = self._input_number(strategy_choose_msg)
+        return strategy
+
+    @staticmethod
+    def info_winner(winner: str):
+        logging.info(winner_msg.format(winner))
+
+    @staticmethod
+    def exception_warnings():
+        logging.info(exception_msg)
+
+    @staticmethod
+    def info_attacking(attacking: str):
+        logging.info(attacking_msg.format(attacking))
